d2afe62fb38bc9b03a66e0015d12318d08d17422
ingridliabakk <ier008@uib.no>
2021-04-29 10:53:53 +0200

    started over

--- Assets/Draggable.cs
+++ Assets/Draggable.cs
@@ -5,18 +5,22 @@ using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-
-    public void OnDrag(PointerEventData eventData) {
-        Debug.Log ("OnDrag");
-    }
+    Transform parentToReturnTo = null;
 
     public void OnBeginDrag(PointerEventData eventData) {
         Debug.Log ("OnBeginDrag");
+        parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        Debug.Log ("OnDrag");
         this.transform.position = eventData.position;
     }
 
     public void OnEndDrag(PointerEventData eventData) {
         Debug.Log ("OnEndDrag");
+        this.transform.SetParent(parentToReturnTo);
     }
   
 }
