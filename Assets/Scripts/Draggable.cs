690558d890617767b0ce0d8ace6236bbc7b1b19e
ingridliabakk <ier008@uib.no>
2021-05-06 11:03:15 +0200

    box can only be dropped in the right slot

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -1,68 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    public Transform parentToReturnTo = null;
-    public enum Slot { RED, BLUE, GREEN };
-    public Slot typeOfItem = Slot.RED;
 
-    public enum Box { RED, BLUE, GREEN };
-    public Box typeOfBox = Box.RED; //if for å finne ut hvilken bokstype?
+	public Transform parentToReturnTo = null;
+	public Transform placeholderParent = null;
 
-    GameObject placeholder = null;
+	GameObject placeholder = null;
 
-    public void OnBeginDrag(PointerEventData eventData) {
-        Debug.Log ("OnBeginDrag");
+	public enum Slot { DEFAULT, RED, BLUE, GREEN };
+	public Slot typeOfItem = Slot.DEFAULT;
 
-        placeholder = new GameObject();
-        placeholder.transform.SetParent(this.transform.parent);
-        LayoutElement le = placeholder.AddComponent<LayoutElement>();
-        le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
-        le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
-        le.flexibleWidth = 0;
-        le.flexibleHeight = 0;
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		Debug.Log("OnBeginDrag");
 
-        placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+		placeholder = new GameObject();
+		placeholder.transform.SetParent(this.transform.parent);
+		LayoutElement le = placeholder.AddComponent<LayoutElement>();
+		le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+		le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+		le.flexibleWidth = 0;
+		le.flexibleHeight = 0;
 
+		placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
 
-        parentToReturnTo = this.transform.parent;
-        this.transform.SetParent(this.transform.parent.parent);
-        GetComponent<CanvasGroup>().blocksRaycasts = false;
+		parentToReturnTo = this.transform.parent;
+		placeholderParent = parentToReturnTo;
+		this.transform.SetParent(this.transform.parent.parent);
 
+		GetComponent<CanvasGroup>().blocksRaycasts = false;
+	}
 
+	public void OnDrag(PointerEventData eventData)
+	{
+		//Debug.Log ("OnDrag");
 
-        //highlight valid slots
-        //have to turn of on onenddrag
-        //DropZone[] zones = GameObject.FindObjectsOfType<DropZone>();
-    }
+		this.transform.position = eventData.position;
 
-    public void OnDrag(PointerEventData eventData) {
-        this.transform.position = eventData.position;
+		if (placeholder.transform.parent != placeholderParent)
+			placeholder.transform.SetParent(placeholderParent);
 
-        //move placeholder when box is hoovered over boxes
-        for(int i =0; i< parentToReturnTo.childCount; i ++) {
-            if(this.transform.position.x < parentToReturnTo.GetChild(i).position.x) {
-                placeholder.transform.SetSiblingIndex(i);
-                break;
-            }
-        }
-    }
+		int newSiblingIndex = placeholderParent.childCount;
 
-    public void OnEndDrag(PointerEventData eventData) {
-        Debug.Log ("OnEndDrag");
-        this.transform.SetParent(parentToReturnTo);
-        this.transform.SetSiblingIndex( placeholder.transform.GetSiblingIndex());
+		for (int i = 0; i < placeholderParent.childCount; i++)
+		{
+			if (this.transform.position.x < placeholderParent.GetChild(i).position.x)
+			{
 
-        GetComponent<CanvasGroup>().blocksRaycasts = true;
+				newSiblingIndex = i;
 
-        Destroy(placeholder);
+				if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
+					newSiblingIndex--;
 
-        //checks whats underneat 
-        //EventSystems.current.RaycastAll(eventData);
-    }
-  
+				break;
+			}
+		}
+
+		placeholder.transform.SetSiblingIndex(newSiblingIndex);
+
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		Debug.Log("OnEndDrag");
+		this.transform.SetParent(parentToReturnTo);
+		this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
+		GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+		Destroy(placeholder);
+	}
 }
--- Assets/Scripts/DropZone.cs
+++ Assets/Scripts/DropZone.cs
@@ -1,22 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 using UnityEngine.EventSystems;
 
-public class DropZone : MonoBehaviour, IDropHandler
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    public Draggable.Slot typeOfItem = Draggable.Slot.RED;
+	public Draggable.Slot typeOfItem = Draggable.Slot.RED;
 
-    public void OnDrop(PointerEventData eventData) {
-        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		//Debug.Log("OnPointerEnter");
+		if (eventData.pointerDrag == null)
+			return;
 
-        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
-        if(d!= null) {
-            if(typeOfItem == d.typeOfItem) {
-                d.parentToReturnTo = this.transform;
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if (d != null)
+		{
+			d.placeholderParent = this.transform;
+		}
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		//Debug.Log("OnPointerExit");
+		if (eventData.pointerDrag == null)
+			return;
+
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if (d != null && d.placeholderParent == this.transform)
+		{
+			d.placeholderParent = d.parentToReturnTo;
+        }
+	}
+
+	public void OnDrop(PointerEventData eventData)
+	{
+		Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if (d != null)
+		{
+			if(typeOfItem == d.typeOfItem)
+            {
+				d.parentToReturnTo = this.transform;
 
             }
-        }
+		}
 
-    }
+	}
 }
